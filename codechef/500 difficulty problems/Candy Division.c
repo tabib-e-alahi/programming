ab845e6c6a0fd9ec9cd5973ed5f9f5f9bafaad8f
tabib-e-alahi <etabib.alahi@gmail.com>
2024-10-14 21:57:35 +0600

    3 more problems added from codechef

--- codechef/500 difficulty problems/Candy Division.c
+++ codechef/500 difficulty problems/Candy Division.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,x;
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    {
+        scanf("%d",&x);
+        if(x % 3== 0)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
 
--- codechef/500 difficulty problems/Roller Coaster.c
+++ codechef/500 difficulty problems/Roller Coaster.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,x,y;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d %d",&x,&y);
+        if(x >= y){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
 
