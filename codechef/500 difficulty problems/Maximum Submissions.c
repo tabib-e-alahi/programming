3d9dc301287826698967892f515de87677de3a83
tabib-e-alahi <etabib.alahi@gmail.com>
2024-10-15 22:38:00 +0600

    2 more problem added from codechef

--- codechef/500 difficulty problems/Maximum Submissions.c
+++ codechef/500 difficulty problems/Maximum Submissions.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t, x, i;
+    scanf("%d",&t);
+    for(i=1; i<=t; i++)
+    {
+        scanf("%d",&x);
+        printf("%d\n", x * 2);
+    }
+    return 0;
+}
+
+
 
--- codechef/500 difficulty problems/Speciality.c
+++ codechef/500 difficulty problems/Speciality.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int t, x, y, z, i;
+    scanf("%d",&t);
+    for(i=1; i<=t; i++)
+    {
+        scanf("%d %d %d",&x, &y, &z);
+        if(x > y && x > z)
+        {
+            printf("Setter\n");
+        }
+        else if(x < y && y > z){
+            printf("Tester\n");
+        }
+        else if(x < z && y < z){
+            printf("Editorialist\n");
+        }
+    }
+    return 0;
+}
+
 
