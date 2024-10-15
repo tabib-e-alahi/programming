1e6a3ea81dd169f63dd21c2ebb0ccc93390c2b3b
tabib-e-alahi <etabib.alahi@gmail.com>
2024-10-15 22:53:06 +0600

    2 more problem added from codechef

--- codechef/500 difficulty problems/Chairs Requirement.c
+++ codechef/500 difficulty problems/Chairs Requirement.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int t, a, b, i;
+    scanf("%d",&t);
+    for(i=1; i<=t; i++)
+    {
+        scanf("%d %d",&a, &b);
+        if(a < b){
+           printf("0\n");
+        }
+        else{
+           printf("%d\n", a-b);
+        }
+
+    }
+    return 0;
+}
 
--- codechef/500 difficulty problems/Four Tickets.c
+++ codechef/500 difficulty problems/Four Tickets.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int t, a i;
+    scanf("%d",&t);
+    for(i=1; i<=t; i++)
+    {
+        scanf("%d",&a);
+        if(4*a <= 1000){
+           printf("YES\n");
+        }
+        else{
+           printf("NO\n");
+        }
+
+    }
+    return 0;
+}
 
