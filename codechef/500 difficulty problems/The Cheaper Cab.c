f622b032b1b80724ad9f174d4046469dbe9007ee
tabib-e-alahi <etabib.alahi@gmail.com>
2024-10-17 20:31:56 +0600

    2 more problems added from codechef

--- codechef/500 difficulty problems/The Cheaper Cab.c
+++ codechef/500 difficulty problems/The Cheaper Cab.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int t, i, x, y;
+    scanf("%d", &t);
+    for(i =1; i <= t; i++)
+    {
+        scanf("%d %d",&x, &y);
+        if(x < y){
+            printf("First\n");
+        }
+        else  if(x > y){
+            printf("Second\n");
+        }
+         else{
+            printf("Any\n");
+        }
+    }
+    return 0;
+}
 
