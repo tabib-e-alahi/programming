5df55de9afdc3cb44eb33fc075f9662e77469393
tabib-e-alahi <etabib.alahi@gmail.com>
2024-10-17 20:27:46 +0600

    2 more problems added from codechef

--- codechef/500 difficulty problems/Discount.c
+++ codechef/500 difficulty problems/Discount.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+    int t, i, x;
+    scanf("%d", &t);
+    for(i =1; i <= t; i++)
+    {
+        scanf("%d",&x);
+        printf("%d\n", 100-x);
+    }
+    return 0;
+}
