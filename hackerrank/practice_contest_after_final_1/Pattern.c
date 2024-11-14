d53d529648bb19e4d9e90a1547820a062b201d49
tabib-e-alahi <etabib.alahi@gmail.com>
2024-11-14 18:36:04 +0600

    2 practice contest problem added

--- hackerrank/practice_contest_after_final_1/Pattern.c
+++ hackerrank/practice_contest_after_final_1/Pattern.c
@@ -1,19 +1,43 @@
-/*#include<stdio.h>
+#include<stdio.h>
+
+void print_function(char ch, int range)
+{
+    for(int j = 1; j <=range; j++)
+        {
+            printf("%c", ch);
+        }
+}
+
 int main()
 {
     int n;
     scanf("%d", &n);
-
-    char hash = '#', hiphen = '-';
-    for(int i = 1; i <= (2 * n) - 1; i++){
-        if(i % 2 != 0){
-                for(int j = 1; j <= (2 * i) - 1; j++){
-                    printf("%c" , hash);
-                }
-                printf("\n");
+    for(int i = 1; i <= n; i++)
+    {
+        //printing space
+        print_function(' ', n-i);
+        if(i % 2 == 0){
+            print_function('-', ( 2 * i  - 1));
         }
+        else{
+            print_function('#', ( 2 * i  - 1));
+        }
+        printf("\n");
+    }
+     for(int i = n-1; i >= 1; i--)
+    {
+        //printing space
+        print_function(' ', n-i);
+        if(i % 2 == 0){
+            print_function('-', ( 2 * i  - 1));
+        }
+        else{
+            print_function('#', ( 2 * i  - 1));
+        }
+        printf("\n");
     }
 
+
 }
-*/
+
 
