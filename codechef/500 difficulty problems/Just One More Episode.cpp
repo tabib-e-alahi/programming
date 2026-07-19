8fcf54176c933658c1a2b7a8e4e4de402b141cbd
tabib-e-alahi <etabib.alahi@gmail.com>
2026-07-20 00:36:39 +0600

    checking

---
+++ codechef/500 difficulty problems/Just One More Episode.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+    {
+        int x;
+        cin >> x;
+        x > 24 ? cout << "YES\n" : cout << "NO\n";
+    }
+    return 0;
+
+}
+
